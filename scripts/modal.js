460e6293460ca1dfebbde1f25411db6a95b7cc63
caljun <calderonjunya0602@gmail.com>
2025-03-25 03:24:51 +0900

    Update modal.js

--- scripts/modal.js
+++ scripts/modal.js
@@ -56,6 +56,22 @@ document.addEventListener("DOMContentLoaded", function () {
                     const oldInput = document.getElementById("postImage");
                     const newInput = oldInput.cloneNode(true);
                     oldInput.parentNode.replaceChild(newInput, oldInput);
+
+                    newInput.addEventListener("change", function (event) {
+                        const file = event.target.files[0];
+                        if (file) {
+                            const reader = new FileReader();
+                            reader.onload = function (e) {
+                                if (window.addPost){
+                                    const name = document.getElementById("postName").value;
+                                    window.addPost(name, e.target.result);
+                                    modal.style.display = "none";
+                                    document.getElementById("postName"). value = "";
+                                }
+                            }
+                        }
+                    })
+                        
                 } else {
                     alert("投稿処理に失敗しました。");
                 }
